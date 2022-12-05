69e809cd2b4263ecd168ee7e14c23b8d0d118342
Coffee Pawz <lauralynneduffy@gmail.com>
2022-12-05 01:37:02 -0500

    POST /api/notes route framework added. Not working right now but frameworks being added then can debug

--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const util = require('util');
+const { v4: uuidv4 } = require('uuid');
+const api = require('express').Router();
 
 //GET request to show saved notes
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
@@ -14,4 +16,27 @@ api.get('/notes', (req, res)=>{
             .then((data) => res.json(data))
         }
     })
-})
+});
+
+// POST /api/notes should receive a new note to save on the request body, 
+//add it to the db.json file, and then return the new note to the client. 
+
+//Use an npm package for giving notes unique IDs 
+// Using https://www.npmjs.com/package/uuid (required above)
+
+api.post('/', (req, res) => {
+    const { title, text } = req.body;
+    const note  = {
+        title,
+        text,
+        id: uuid(),
+    };
+
+    util.promisify(fs.readFile("./db/db.json"))
+    .then(data => JSON.parse(data))
+    .then((data) => {
+        data.push(note)
+        util.promisify(fs.writeFile(data),JSON.stringify(data)) 
+        });
+    }
+)
