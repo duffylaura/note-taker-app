7262b84c39bf092fb709c56822c9e8c8e7445bcd
Coffee Pawz <lauralynneduffy@gmail.com>
2022-12-05 20:53:15 -0500

    Stuck, ucaught (in promise) syntax errors I cannot resolve

--- routes/notes.js
+++ routes/notes.js
@@ -1,53 +1,41 @@
 const fs = require('fs');
-var uniqid = require("uniqid");
+const util = require('util');
+const { v4: uuidv4 } = require("uuid");
 const api = require('express').Router();
 const database = require('../db/db.json');
 
 //GET request to show saved notes
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 // use fs read file
-api.get('/notes', (req, res)=>{
-    res.json(database);
-});
+api.get("/notes", (req, res) => {
+    // util.promisify(fs.readFile("../db/db.json")
+    //   .then((data) => JSON.parse(data))
+    //   .then((json) => {
+    //     res.json(json);
+    //   }));
+    const readFile = util.promisify(fs.readFile);
+    async function doFile() {
+        try {
+            const data = await readFile('../db/db.json', 'utf8');
+            console.log(data);
+        } catch (err) {
+            console.log('Error', err);
+        }
+    }
+    doFile();
+  });
 
 // POST /api/notes should receive a new note to save on the request body, 
 //add it to the db.json file, and then return the new note to the client. 
+//Use an npm package for giving notes unique IDs (required above)
 
-//Use an npm package for giving notes unique IDs 
-// Using https://www.npmjs.com/package/uniqid (required above)
-
-api.post('/', (req, res) => {
+api.post('/notes', async (req, res) => {
     const { title, text } = req.body;
-    if (title && text) {
-        const newNote  = {
-            title,
-            text,
-            id: uniqid(),
-        };
-        //fs.readFile( filename, encoding, callback_function )
-        fs.readFile('../db/db.json', 'utf8',(err,data)=>{
-            if (err) {console.error(err)}
-            else {
-                const newDatabase = JSON.parse(data);
-                newDatabase.push(newNote);
-                database = newDatabase;
-                // fs.writeFile( file, data, options, callback )
-                fs.writeFile('../db/db.json', JSON.stringify(newDatabase), 'utf8', (err) => {
-                    if (err) {console.err(err)}
-                    else {console.info('Successful writeFile operation!')}
-                })
-            }
-        });
-
-        const response = {status: 'success', body: newNote };
-        
-        res.json(response);
-    }
-
-    else {
-        res.json('Error! Cannot make post request.')
-    }
+    const newNote  = {title, text, id: uuidv4()};
+    database.push(newNote);
+    await fs.writeFile('../db/db.json', JSON.stringify(database,null,4));
+    const response = {status: 'success', body: JSON.stringify(database)};
+    res.json(response);
 });
-
 
 module.exports = api; 
