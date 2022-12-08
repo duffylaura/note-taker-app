26a8baf9d13d1086f35e207a4e87b54ec92e3c66
Coffee Pawz <lauralynneduffy@gmail.com>
2022-12-08 12:02:20 -0500

    FINALLY GOT ITgit add . The *Unexpected token < in JSON at position 0* has been alleviated and notes properly save to page

--- routes/notes.js
+++ routes/notes.js
@@ -1,43 +1,42 @@
-const fs = require('fs');
-const { v4: uuidv4 } = require("uuid");
-const api = require('express').Router();
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const { 
+    readFromFile,
+    readAndAppend, 
+    } = require('../helpers/fsUtils');
+
 
 //GET request to show saved notes
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
-// use fs read file
-api.get("/notes", (req, res) => {
-
-    fs.readFile('../db/db.json', "utf8", function(err, data){
-        if(err) throw err;
-        var resultArray = JSON.parse(data);//do operation on data that generates say resultArray;
-        res.send(resultArray);
-    });
-}); 
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => 
+    res.json(JSON.parse(data)));
+  });
 
 // POST /api/notes should receive a new note to save on the request body, 
 //add it to the db.json file, and then return the new note to the client. 
 //Use an npm package for giving notes unique IDs (required above)
 
-api.post('/notes', (req, res) => {
+notes.post('/', (req, res) => {
+    console.log(req.body);
     const { title, text } = req.body;
-    const newNote  = {title, text, id: uuidv4()};
-
     //read file of the data base, to check what is currently in there
     //Parse information (parse out of string into object)
     //THEN push 
+    if (title && text) {
+        const newNote = {
+            title, text, note_id: uuidv4(), 
+        };
+        readAndAppend(newNote, './db/db.json');
+        const response = {
+            status: 'success', 
+            body: newNote, 
+        };
+        res.json(response);
+    } else {
+        res.json('Error in adding note'); 
+    }
 
-    fs.readFile('../db/db.json', "utf8", function(err, data){
-        if(err) throw err;
-    
-        var resultArray = JSON.parse(data);//do operation on data that generates say resultArray;
-
-        resultArray = resultArray.push(newNote); 
-
-        fs.writeFile('../db/db.json', resultArray, 
-            (err) => {if (err) console.log('POST error: '+err)
-                        else {console.log("File written successfully")}}
-        ) 
-    });
 });
 
-module.exports = api; 
+module.exports = notes; 
